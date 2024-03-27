93bbc7ad23affb98eb538bba305e068214cfd38c
Pooja Babusingh <68894211+PoojaBabusingh@users.noreply.github.com>
2024-03-27 13:50:34 +0530

    first commit

---
+++ Sources/lastios/lastios.swift
@@ -0,0 +1,11 @@
+// The Swift Programming Language
+// https://docs.swift.org/swift-book
+
+
+public class lastios{
+    public init(){}
+    public func multiplyLib(_ a: Int, _ b: Int)-> Int{
+        print("Result is from library ", a*b)
+        return a*b;
+    }
+}
